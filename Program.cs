7cbeeed5033822ef13b485822c8f80d08737e676
Marcos Costa <mcs1901@outlook.com>
2018-07-05 17:38:06 +0000

    Making Web Requests

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace rest_net
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WebAPIClient
 {
     class Program
     {
+	private static readonly HttpClient client = new HttpClient();
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+	{
+		ProcessRepositories().Wait();
         }
+
+	private static async Task ProcessRepositories()
+	{
+		client.DefaultRequestHeaders.Accept.Clear();
+		client.DefaultRequestHeaders.Accept.Add(
+			new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")
+		);
+		client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+		var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+		
+		var msg = await stringTask;
+		Console.Write(msg);
+		
+	}
     }
 }
